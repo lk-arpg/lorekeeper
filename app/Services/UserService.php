48a76c790c53ba8bcb272ed9628958be1d8e3f98
moif <moiftab@gmail.com>
2025-10-17 23:05:46 -0400

    Small bug fixes

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -259,7 +259,7 @@ class UserService extends Service
                 $qsubmissionManager = new QueueSubmissionManager;
                 $qsubmissions = QueueSubmission::where('user_id', $user->id)->where('status', 'Pending')->get();
                 foreach($qsubmissions as $qsubmission)
-                    $qsubmissionManager->rejectSubmission(['submission' => $qsubmission, 'staff_comments' => 'User has been banned from site activity.']);
+                    $qsubmissionManager->rejectSubmission(['submission' => $qsubmission, 'staff_comments' => 'User has been banned from site activity.'], $staff);
 
                 UserUpdateLog::create(['staff_id' => $staff->id, 'user_id' => $user->id, 'data' => json_encode(['is_banned' => 'Yes', 'ban_reason' => isset($data['ban_reason']) ? $data['ban_reason'] : null]), 'type' => 'Ban']);
 
--- routes/lorekeeper/members.php
+++ routes/lorekeeper/members.php
@@ -99,7 +99,6 @@ Route::group(['prefix' => 'character', 'namespace' => 'Characters'], function() 
 
     Route::post('{slug}/approval', 'CharacterController@postCharacterApproval');
     Route::get('{slug}/approval', 'CharacterController@getCharacterApproval');
-    Route::post('{slug}/icon', 'CharacterController@postIcon');
 });
 Route::group(['prefix' => 'myo', 'namespace' => 'Characters'], function() {
     Route::get('{id}/profile/edit', 'MyoController@getEditCharacterProfile');

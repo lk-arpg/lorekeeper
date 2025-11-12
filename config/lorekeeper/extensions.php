a70677e582f994a33d5c2940f5bb8286150ced4c
Newt <scuffednewt@gmail.com>
2025-11-12 13:33:00 +0000

    Update config/lorekeeper/extensions.php
    Co-authored-by: Mercury <63693594+itinerare@users.noreply.github.com>

--- config/lorekeeper/extensions.php
+++ config/lorekeeper/extensions.php
@@ -87,7 +87,7 @@ return [
     'organised_traits_dropdown' => [
         'enable'           => 1,
         'display_species'  => 0, // displays species
-        'display_subtype'  => 0, // displays subtype, SPECIES DISPLAY MUST ALSO BE ENABLED FOR THIS TO WORK.
+        'display_subtype'  => 0, // displays subtype. NOTE: species display must also be enabled for this to work.
         'rarity'           => [
             'enable'         => 1, // If enabled, displays trait rarity in the dropdown.
             'sort_by_rarity' => 1, // If enabled, sorts traits by rarity in the dropdown.

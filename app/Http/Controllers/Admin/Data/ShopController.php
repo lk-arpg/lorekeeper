9a6b6baa2a0373ef6eb620ee4384e83e33c2ef23
ScuffedNewt <ScuffedNewt@users.noreply.github.com>
2025-11-18 23:25:22 +0000

    refactor: fix PHP styling

--- app/Http/Controllers/Admin/Data/ShopController.php
+++ app/Http/Controllers/Admin/Data/ShopController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin\Data;
 
 use App\Http\Controllers\Controller;
-use App\Models\Currency\Currency;
 use App\Models\Item\Item;
 use App\Models\Shop\Shop;
 use App\Models\Shop\ShopStock;

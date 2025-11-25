b34911fbd4fc13d9b42ffc600a8df21cac9d4d08
Fulmine <jake@pals.gay>
2025-11-25 03:56:14 +0100

    move extension to credits page

--- config/lorekeeper/ext-tracker/multiple_trait_subtypes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'wiki_key' => 'Multiple_Trait_Subtypes',
-    'creators' => json_encode([
-        'Fulmn' => 'https://github.com/Draconizations/',
-    ]),
-    'version'  => '1.0.0',
-];

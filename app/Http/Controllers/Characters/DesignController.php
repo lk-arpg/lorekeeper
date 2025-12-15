fcf440c8cd4571d6a662237d3e111ed308f66f2b
Speedy <speedydvv@gmail.com>
2025-12-15 20:25:02 +0100

    Fixing the nit

--- app/Http/Controllers/Characters/DesignController.php
+++ app/Http/Controllers/Characters/DesignController.php
@@ -235,7 +235,7 @@ class DesignController extends Controller {
             'specieses' => ['0' => 'Select Species'] + Species::visible()->orderBy('sort', 'DESC')->pluck('name', 'id')->toArray(),
             'subtypes'  => Subtype::visible()->where('species_id', '=', $r->species_id)->orderBy('sort', 'DESC')->pluck('name', 'id')->toArray(),
             'rarities'  => ['0' => 'Select Rarity'] + Rarity::orderBy('sort', 'DESC')->pluck('name', 'id')->toArray(),
-            'features'  => Feature::getDropdownItems(Auth::user()->hasPower('edit_data'), $r->species_id),
+            'features'  => Feature::getDropdownItems(0, $r->species_id),
         ]);
     }
 

7eeaea554b88015bb0b631a9fef01db52ca8fe54
SpeedyD <SpeedyD@users.noreply.github.com>
2026-01-26 04:39:53 +0000

    refactor: fix blade formatting

--- resources/views/admin/loot_tables/loot_tables.blade.php
+++ resources/views/admin/loot_tables/loot_tables.blade.php
@@ -30,68 +30,68 @@
                 ['class' => 'form-control'],
             ) !!}
         </div>
-            <div class="form-group ml-3">
-                {!! Form::select(
-                    'sort',
-                    [
-                        'newest' => 'Created Newest',
-                        'oldest' => 'Created Oldest',
-                        'alpha' => 'Sort Alphabetically (A-Z) on Name',
-                        'alpha-reverse' => 'Sort Alphabetically (Z-A) on Name',
-                        'alpha-dn' => 'Sort Alphabetically (A-Z) on DisplayName',
-                        'alpha-dn-reverse' => 'Sort Alphabetically (Z-A) on DisplayName',
-                    ],
-                    Request::get('sort') ?: 'Created Oldest',
-                    ['class' => 'form-control'],
-                ) !!}
-            </div>
-            <div class="form-group ml-3">
-                {!! Form::submit('Search', ['class' => 'btn btn-primary']) !!}
-            </div>
-            {!! Form::close() !!}
+        <div class="form-group ml-3">
+            {!! Form::select(
+                'sort',
+                [
+                    'newest' => 'Created Newest',
+                    'oldest' => 'Created Oldest',
+                    'alpha' => 'Sort Alphabetically (A-Z) on Name',
+                    'alpha-reverse' => 'Sort Alphabetically (Z-A) on Name',
+                    'alpha-dn' => 'Sort Alphabetically (A-Z) on DisplayName',
+                    'alpha-dn-reverse' => 'Sort Alphabetically (Z-A) on DisplayName',
+                ],
+                Request::get('sort') ?: 'Created Oldest',
+                ['class' => 'form-control'],
+            ) !!}
         </div>
+        <div class="form-group ml-3">
+            {!! Form::submit('Search', ['class' => 'btn btn-primary']) !!}
+        </div>
+        {!! Form::close() !!}
+    </div>
 
-        @if (!count($tables))
-            <p>No loot tables found.</p>
-        @else
-            {!! $tables->render() !!}
-            <div class="mb-4 logs-table">
-                <div class="logs-table-header">
-                    <div class="row">
-                        <div class="col-3 col-md-2">
-                            <div class="logs-table-cell">ID</div>
-                        </div>
-                        <div class="col-3 col-md-4">
-                            <div class="logs-table-cell">Name</div>
-                        </div>
-                        <div class="col-6 col-md-5">
-                            <div class="logs-table-cell">Display Name</div>
-                        </div>
+    @if (!count($tables))
+        <p>No loot tables found.</p>
+    @else
+        {!! $tables->render() !!}
+        <div class="mb-4 logs-table">
+            <div class="logs-table-header">
+                <div class="row">
+                    <div class="col-3 col-md-2">
+                        <div class="logs-table-cell">ID</div>
+                    </div>
+                    <div class="col-3 col-md-4">
+                        <div class="logs-table-cell">Name</div>
+                    </div>
+                    <div class="col-6 col-md-5">
+                        <div class="logs-table-cell">Display Name</div>
                     </div>
                 </div>
-                <div class="logs-table-body">
-                    @foreach ($tables as $table)
-                        <div class="logs-table-row">
-                            <div class="row flex-wrap">
-                                <div class="col-3 col-md-2">
-                                    <div class="logs-table-cell">#{{ $table->id }}</div>
-                                </div>
-                                <div class="col-3 col-md-4">
-                                    <div class="logs-table-cell">{{ $table->name }}</div>
-                                </div>
-                                <div class="col-3 col-md-5">
-                                    <div class="logs-table-cell">{!! $table->display_name !!}</div>
-                                </div>
-                                <div class="col-3 col-md-1 text-right">
-                                    <div class="logs-table-cell"><a href="{{ url('admin/data/loot-tables/edit/' . $table->id) }}" class="btn btn-primary py-0 px-2">Edit</a></div>
-                                </div>
+            </div>
+            <div class="logs-table-body">
+                @foreach ($tables as $table)
+                    <div class="logs-table-row">
+                        <div class="row flex-wrap">
+                            <div class="col-3 col-md-2">
+                                <div class="logs-table-cell">#{{ $table->id }}</div>
+                            </div>
+                            <div class="col-3 col-md-4">
+                                <div class="logs-table-cell">{{ $table->name }}</div>
+                            </div>
+                            <div class="col-3 col-md-5">
+                                <div class="logs-table-cell">{!! $table->display_name !!}</div>
+                            </div>
+                            <div class="col-3 col-md-1 text-right">
+                                <div class="logs-table-cell"><a href="{{ url('admin/data/loot-tables/edit/' . $table->id) }}" class="btn btn-primary py-0 px-2">Edit</a></div>
                             </div>
                         </div>
-                    @endforeach
-                </div>
+                    </div>
+                @endforeach
             </div>
-            {!! $tables->render() !!}
-            <div class="text-center mt-4 small text-muted">{{ $tables->total() }} result{{ $tables->total() == 1 ? '' : 's' }} found.</div>
-        @endif
+        </div>
+        {!! $tables->render() !!}
+        <div class="text-center mt-4 small text-muted">{{ $tables->total() }} result{{ $tables->total() == 1 ? '' : 's' }} found.</div>
+    @endif
 
-    @endsection
+@endsection

92600924de95cd528327595ec7c4676cb22e15b3
Kazimir <stokoriel@gmail.com>
2026-03-09 11:09:08 -0500

    fix: change character currency log query with() back to sender/recipient.rank

--- app/Models/Character/Character.php
+++ app/Models/Character/Character.php
@@ -470,9 +470,9 @@ class Character extends Model {
         $character = $this;
         $query = CurrencyLog::with('currency')->where(function ($query) use ($character) {
             $query->where(function ($query) use ($character) {
-                $query->with('sender')->where('sender_type', 'Character')->where('sender_id', $character->id)->where('log_type', '!=', 'Staff Grant');
+                $query->with('sender.rank')->where('sender_type', 'Character')->where('sender_id', $character->id)->where('log_type', '!=', 'Staff Grant');
             })->orWhere(function ($query) use ($character) {
-                $query->with('recipient')->where('recipient_type', 'Character')->where('recipient_id', $character->id)->where('log_type', '!=', 'Staff Removal');
+                $query->with('recipient.rank')->where('recipient_type', 'Character')->where('recipient_id', $character->id)->where('log_type', '!=', 'Staff Removal');
             });
         })->orderBy('id', 'DESC');
 

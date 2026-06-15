67f876923d17e8cc2a9ec98d7d7446763c856154
SpeedyD <speedydvv@gmail.com>
2026-06-15 05:12:18 +0200

    Feat(world): Seperated Trait Page

--- config/lorekeeper/extensions.php
+++ config/lorekeeper/extensions.php
@@ -140,6 +140,9 @@ return [
 
     // Unmerge Item Page and Item Entry - Speedy
     'unmerge_item_page_and_entry' => 0, // If enabled, uses the html on world/item_page.blade.php instead of the include that links to world/_item_entry.blade.php
+    
+    // Unmerge Trait Page and Trait Entry - Speedy
+    'unmerge_feature_page_and_entry' => 0, // If enabled, uses the html on world/feature_page.blade.php instead of the include that links to world/_feature_entry.blade.php
 
     // Show Species-only traits in dropdown - Speedy
     'show_exclusively_species_traits_in_dropdown' => 0, // If enabled, will only show traits from the associated species as well as traits that aren't species-limited in the dropdown menus.

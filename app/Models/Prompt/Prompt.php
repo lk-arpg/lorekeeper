94f60b4d7090c6c1c2dd976f53334df6586d3c25
perappu <perappu@users.noreply.github.com>
2026-07-16 20:37:39 +0000

    refactor: fix PHP styling

--- app/Models/Prompt/Prompt.php
+++ app/Models/Prompt/Prompt.php
@@ -334,11 +334,11 @@ class Prompt extends Model {
      */
     public function getCount($user, $characters = null) {
         // filter the submissions by hour/day/week/etc and returns count
-        if($characters && count($characters)) {
+        if ($characters && count($characters)) {
             $ids = $characters->pluck('id');
             $submissions = Submission::submitted($this->id, $user->id)->whereHas('characters', function ($q) use ($ids) {
-                    $q->whereIn('character_id', $ids);
-                })->get();
+                $q->whereIn('character_id', $ids);
+            })->get();
         } else {
             $submissions = Submission::submitted($this->id, $user->id)->get();
         }
